178a6f1a3879e999b64308c0acc116db45f90e6c
john <johnm.aiken@gmail.com>
2021-03-29 11:31:20 +0200

    updated datahelper, updated paper tables

--- DataHelper.py
+++ DataHelper.py
@@ -5,15 +5,45 @@ class eclass_data:
     """
     A container class for the eclass data set that makes pulling chunks
     of the data easier
-
+    
+    All functions return pandas dataframes and can thus be interacted with
+    exactly as with a pandas dataframe.
+    
+    Class attributes include the raw data sets for pre, post, and cis data.
+    Class attributes also include the unique IDs for intro and BFY students
+    as well as question IDs for the survey itself to extract out only survey
+    data.
+    
+    TODO : make unit test for unique_students 
     """
     
     def __init__(self, route=''):
         self.pre = pd.read_csv(route + 'anon_pre.csv')
         self.post = pd.read_csv(route +'anon_post.csv')
         self.cis = pd.read_csv(route +'anon_cis.csv')
+        self.cis['StartDate'] = pd.to_datetime(self.cis['StartDate'])
         self.first_year_id = self.cis[self.cis.Q18=='First year (introductory) lab'].ResponseId
         self.buffy_id = self.cis[self.cis.Q18=='Beyond the first year lab'].ResponseId
+        self.pre_survey_question_ids = ['q01a', 'q01b',
+       'q02a', 'q02b', 'q03a', 'q03b', 'q04a', 'q04b', 'q05a', 'q05b',
+       'q06a', 'q06b', 'q07a', 'q07b', 'q09a', 'q09b', 'q10a', 'q10b',
+       'q11a', 'q11b', 'q12a', 'q12b', 'q13a', 'q13b', 'q14a', 'q14b',
+       'q15a', 'q15b', 'q16a', 'q16b', 'q17a', 'q17b', 'q18a', 'q18b',
+       'q19a', 'q19b', 'q20a', 'q20b', 'q21a', 'q21b', 'q22a', 'q22b',
+       'q23a', 'q23b', 'q24a', 'q24b', 'q25a', 'q25b', 'q26a', 'q26b',
+       'q27a', 'q27b', 'q28a', 'q28b', 'q29a', 'q29b', 'q30a', 'q30b',
+       'q31a', 'q31b',] # questions 40a and 40b are removed from this list since they are the discard questions
+        self.post_survey_question_ids = ['q01a', 'q01b', 'q01c', 'q02a',
+       'q02b', 'q02c', 'q03a', 'q03b', 'q03c', 'q04a', 'q04b', 'q04c',
+       'q05a', 'q05b', 'q05c', 'q06a', 'q06b', 'q06c', 'q07a', 'q07b',
+       'q07c', 'q09a', 'q09b', 'q09c', 'q10a', 'q10b', 'q10c', 'q11a',
+       'q11b', 'q11c', 'q12a', 'q12b', 'q12c', 'q13a', 'q13b', 'q13c',
+       'q14a', 'q14b', 'q14c', 'q15a', 'q15b', 'q15c', 'q16a', 'q16b',
+       'q16c', 'q17a', 'q17b', 'q17c', 'q18a', 'q18b', 'q18c', 'q19a',
+       'q19b', 'q19c', 'q20a', 'q20b', 'q20c', 'q21a', 'q21b', 'q21c',
+       'q22a', 'q22b', 'q22c', 'q23a', 'q23b', 'q24a', 'q24b', 'q25a',
+       'q25b', 'q26a', 'q26b', 'q27a', 'q27b', 'q28a', 'q28b', 'q28c',
+       'q29a', 'q29b', 'q30a', 'q30b', 'q30c', 'q31a', 'q31b', ]  # questions 40a and 40b are removed from this list since they are the discard questions
         
         
     def _get_data(self, pre_post_cis=None, intro_buffy=None, unique_students=False):
@@ -107,12 +137,60 @@ class eclass_data:
         post = self.get_buffy_post(unique_students=unique_students)
         return pre.merge(post, on=['anon_student_id', 'ResponseId'], suffixes=['_pre', '_post'])
     
+    def get_pre(self, unique_students=False):
+        """
+        Returns pre-survey data for all students regardless of intro or bfy status
+        
+        TODO : make unit test
+        """
+        intro = self.get_intro_pre(unique_students=unique_students)
+        bfy = self.get_buffy_pre(unique_students=unique_students)
+        return pd.concat([intro, bfy])
+    
+    def get_post(self, unique_students=False):
+        """
+        Returns post survey data for all students regardless of intro or bfy status
+        
+        TODO : make unit test
+        """
+        intro = self.get_intro_post(unique_students=unique_students)
+        bfy = self.get_buffy_post(unique_students=unique_students)
+        return pd.concat([intro, bfy])
+    
+    def get_matched(self, unique_students=False):
+        """
+        Returns survey data for all matched students regardless of intro or bfy status
+        
+        TODO : make unit test
+        """
+        intro = self.get_intro_matched(unique_students=unique_students)
+        bfy = self.get_buffy_matched(unique_students=unique_students)
+        return pd.concat([intro, buffy])
+    
     def get_cis_intro(self):
+        """
+        Returns the CIS data for intro students
+        
+        TODO : make unit tests
+        """
         return self._get_data(pre_post_cis='cis', intro_buffy='intro')
 
     def get_cis_buffy(self):
+        """
+        Returns the CIS data for bfy students
+        
+        TODO : make unit tests
+        """
         return self._get_data(pre_post_cis='cis', intro_buffy='bfy')
 
+    def get_cis(self):
+        """
+        Returns full CIS table
+        """
+        intro = self.get_cis_intro()
+        bfy = self.get_cis_buffy()
+        return pd.concat([intro, bfy])
+    
 if __name__ == "__main__":
     import doctest
     doctest.testmod(extraglobs={'e': eclass_data()})
